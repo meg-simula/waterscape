6e84140f0ef687945b4df69311fbfd3ad58d9f43
elepiersan <eleonora@simula.no>
2017-01-18 12:33:13 +0100

    added a file taht converts amira file to xml. Also for subdomains

--- src/amira2fenics_bi18.py
+++ src/amira2fenics_bi18.py
@@ -5,20 +5,23 @@ from dolfin_utils.meshconvert import xml_writer
 import re 
 
 
-def run(input, output): 
+def run(input): 
   """ 
   convert between amira and fenics implemented as state machine 
   0 = look for starters 
   1 = vertex 
   2 = cells 
+  3 = labels
   """
   state = 0 
-  ifile = open(ifilename)
-  ofile = open(ofilename, "w")
-
+  ifile = open(input[0])
+  ofile = open(input[1], "w")
+  ofile_sub = open(input[2], "w")
+  
   vertices =[] 
   cells = []
-
+  labels = []
+  
   index_vertices = 0
   index_cells = 0
   while 1:
@@ -31,6 +34,7 @@ def run(input, output):
       state = 2  
     elif line.startswith("@3"):
       state = 3
+    elif line.startswith("@4"):
       break
     
     elif state == 1:   
@@ -38,19 +42,21 @@ def run(input, output):
     #-37.168381 50.233715 -12.345407
       # print line.strip().split()
       index_vertices += 1
-      print state
-      
       x, y, z = line.strip().split() 
       vertices.append((index_vertices,x,y,z))
 
     elif state == 2:   
     # example: 
 #    *tetra4(1,1,38366,38367,9253,43441)
-      print state
-
       index_cells += 1
       v1, v2, v3, v4= line.strip().split() 
       cells.append((index_cells,v1,v2,v3,v4))
+
+    elif state == 3:   
+    # example: 
+#    *tetra4(1,1,38366,38367,9253,43441)
+      label = line.strip() 
+      labels.append(label)
 
   print "out of loop"
   xml_writer.write_header_mesh(ofile, "tetrahedron", 3) 
@@ -71,6 +77,23 @@ def run(input, output):
     counter += 1 
   xml_writer.write_footer_cells(ofile)
 
+
+  header = ("""<?xml version="1.0"?>
+<dolfin xmlns:dolfin="http://fenicsproject.org">
+  <mesh_function>
+    <mesh_value_collection name="" type="uint" dim=\"%d" size=\"%d">\n""") % (2, len(cells))
+  
+  ofile_sub.write(header)
+  
+  counter=0
+  for c in cells: 
+    if not counter == int(c[0])-1: print "Warning! Numbering assumption invalidated", counter, c[0]
+    xml_writer.write_entity_meshvaluecollection(ofile_sub, 1, int(c[0])-1, int(labels[counter]), local_entity=2) 
+
+    counter += 1
+    
+  xml_writer.write_footer_meshvaluecollection(ofile_sub)
+  xml_writer.write_footer_meshfunction(ofile_sub)
   xml_writer.write_footer_mesh(ofile)
  
 #   not handled: 
@@ -83,9 +106,10 @@ def run(input, output):
 #    if line.startswith("
 
 if __name__ == "__main__": 
+
   ifilename=sys.argv[1]
   ofilename=sys.argv[2]
+  ofilename_sub=sys.argv[3]
 
-
-  run(ifilename, ofilename)
+  run((ifilename, ofilename, ofilename_sub))
 
